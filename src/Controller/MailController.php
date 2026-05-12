f57f9fbc47e98a24e81fd9b79dda3eeb251cc517
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2026-05-12 13:51:01 +0200

    mailpit fonctionne y compris avec la pièce jointe !

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -13,14 +13,21 @@ final class MailController extends AbstractController
     #[Route('/mail', name: 'app_mail')]
     public function index(MailerInterface $mailer): Response
     {
-        $email = (new Email())
-            ->from('test@test.com')
-            ->to('ms.duplouichiscod@gmail.com')
-            ->subject('Test Mailpit')
-            ->text('Bonjour depuis Symfony');
+        $file_resource = tmpFile();    //Ouvre un fichier temporaire (similaire à fopen)
+        fwrite($file_resource, "Ceci est le contenu du fichier à attacher au mail !");
+
+        $email = new Email();
+        $email->from("website@cool.com")
+            ->to("ms.duplouichiscod@gmail.com")
+            ->subject("Ceci est un mail Test sujet")
+            ->attach($file_resource, "bienvenuecheznous.txt")    // ++ AJOUTER UNE PIÈCE JOINTE.
+            ->text("Ceci est un mail Test texte");
 
         $mailer->send($email);
 
-        return new Response('Mail envoyé');
+
+        return $this->render('mail/index.html.twig', [
+            'controller_name' => 'MailController',
+        ]);
     }
 }
