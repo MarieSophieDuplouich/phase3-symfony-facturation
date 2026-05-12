a3ff8b5c616f9f7d5e61dfc64fd55ffbf3872142
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2026-05-12 10:27:36 +0200

    mailer et gotenberg installés perfection

---
+++ config/packages/mailer.php
@@ -0,0 +1,10 @@
+// config/packages/mailer.php
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+return App::config([
+    'framework' => [
+        'mailer' => [
+            'dsn' => env('MAILER_DSN'),
+        ],
+    ],
+]);
