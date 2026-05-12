554ccb25cd7225f3e3e879e831025461fe079e18
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2026-05-12 14:41:03 +0200

    generate pdf mailpit et email avec pièce jointe fonctionnent !

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -8,26 +8,37 @@ use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
+use Sensiolabs\GotenbergBundle\GotenbergPdfInterface;
+use Sensiolabs\GotenbergBundle\Processor\TempfileProcessor;
+
 final class MailController extends AbstractController
 {
     #[Route('/mail', name: 'app_mail')]
-    public function index(MailerInterface $mailer): Response
-    {
-        $file_resource = tmpFile();    //Ouvre un fichier temporaire (similaire à fopen)
-        fwrite($file_resource, "Ceci est le contenu du fichier à attacher au mail !");
+    public function index(
+        MailerInterface $mailer,
+        GotenbergPdfInterface $gotenberg
+    ): Response {
 
-        $email = new Email();
-        $email->from("website@cool.com")
-            ->to("ms.duplouichiscod@gmail.com")
-            ->subject("Ceci est un mail Test sujet")
-            ->attach($file_resource, "bienvenuecheznous.txt")    // ++ AJOUTER UNE PIÈCE JOINTE.
-            ->text("Ceci est un mail Test texte");
+        $filePdf = $gotenberg->html()
+            ->content('test/index.html.twig')
+            ->processor(new TempfileProcessor())
+            ->generate()
+            ->process();
+
+        $email = (new Email())
+            ->from('website@cool.com')
+            ->to('ms.duplouichiscod@gmail.com')
+            ->subject('Mail !!!!!!!!!!!! avec PDF')
+            ->text('Bonjour, voici votre PDF en pièce jointe.')
+
+            ->attach(
+                $filePdf,
+                'document.pdf',
+                'application/pdf'
+            );
 
         $mailer->send($email);
 
-
-        return $this->render('mail/index.html.twig', [
-            'controller_name' => 'MailController',
-        ]);
+        return $this->render('mail/index.html.twig');
     }
 }
